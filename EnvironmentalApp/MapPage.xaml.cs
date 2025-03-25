5c88bd33e66bf43dc8ca322b0ca4af3e2563a972
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2025-03-25 16:54:18 +0000

    Implemented Map functionality :)

--- EnvironmentalApp/MapPage.xaml.cs
+++ EnvironmentalApp/MapPage.xaml.cs
@@ -1,9 +1,46 @@
 namespace EnvironmentalApp;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 
 public partial class MapPage : ContentPage
 {
-	public MapPage()
-	{
-		InitializeComponent();
-	}
+    void OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine($"MapClick: {e.Location.Latitude}, {e.Location.Longitude}");
+
+        var p = new Pin()
+        {
+            Location = e.Location,
+            Label = "A pin",
+            Address = "A place"
+        };
+        myMap.Pins.Add(p);
+    }
+
+    public MapPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var p = new Pin()
+        {
+            Location = new Location(40, 2),
+            Label = "Test",
+            Address = "Test2"
+        };
+
+        p.MarkerClicked += P_MarkerClicked;
+
+        myMap.Pins.Add(p);
+
+        myMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Location(40, 2), Distance.FromKilometers(10)));
+    }
+
+    private void P_MarkerClicked(object sender, PinClickedEventArgs e)
+    {
+        DisplayAlert("Test1", "Test2", "Test3");
+    }
 }
--- EnvironmentalApp/MauiProgram.cs
+++ EnvironmentalApp/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Maps;
+using System.Diagnostics;
 
 namespace EnvironmentalApp
 {
@@ -9,14 +13,18 @@ namespace EnvironmentalApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+#if WINDOWS
+        Debug.WriteLine("Windows");
+        builder.UseMauiCommunityToolkitMaps("1g5lpw6it9LDTBpHeAQzM9nPcgtOYHr3lEvSuZ4G62HRjBovPneXJQQJ99BCACi5YpzcXOIAAAAgAZMP2wy5");
+#endif
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
