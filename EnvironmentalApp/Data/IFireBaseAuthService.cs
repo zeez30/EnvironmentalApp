3c04f65f82c4cf76d767455ab5f9c8ebec760e27
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2025-04-19 14:47:50 +0100

    Login Framework

---
+++ EnvironmentalApp/Data/IFireBaseAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace EnvironmentalApp.Data
+{
+    public interface IFirebaseAuthService
+    {
+        Task<bool> SignInWithEmailAndPasswordAsync(string email, string password);
+        Task SignOutAsync();
+        bool IsSignedIn();
+    }
+}
--- EnvironmentalApp/Platforms/Android/MainActivity.cs
+++ EnvironmentalApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,21 @@
 ﻿using Android.App;
-using Android.Content.PM;
 using Android.OS;
+using Firebase;
 
 namespace EnvironmentalApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Label = "EnvironmentalApp", MainLauncher = true)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Init Firebase using native context
+            if (FirebaseApp.InitializeApp(this) == null)
+            {
+                FirebaseApp.InitializeApp(this);
+            }
+        }
     }
 }
